fddc82db19ef55eabe147c14efab053bee3e6465
Logan Brown <logan.brown@tylertech.com>
2024-04-18 14:19:34 -0400

    Added email service and database controller

--- webserver/server.ts
+++ webserver/server.ts
@@ -4,15 +4,27 @@ const port = 80;
 const app = express();
 import query from "../controllers/database";
 import log from "../modules/logger";
+import * as email from "../services/email";
 
 // Test the database connection
 query("SELECT 1 + 1 AS solution", [])
-  .then(() => log.info("Database connection successful"))
-  .catch(err => {
-    log.error("Database connection failed")
-    log.error(err)
-    process.exit(1)
-  })
+  .then(() => log.success("Database connection successful"))
+  .catch((err) => {
+    log.error("Database connection failed");
+    log.error(err);
+    process.exit(1);
+  });
+
+email.send(
+  process.env.EMAIL_TEST as string,
+  "Test Email",
+  "This is a test email from the web server."
+)
+.then(() => log.success("Email service is available"))
+.catch((e: Error) => {
+  log.error(`Email service is not available: ${e.message}`);
+  throw `Email service is not available: ${e.message}`;
+});
 
 // Middleware
 app.use(express.json());
