b21c7e815dfcea5c771418b8f77296b68cfe338a
Logan Brown <logan.brown@tylertech.com>
2024-11-21 23:01:18 -0500

    Refactor transpile.ts

--- src/utility/transpiler.ts
+++ src/utility/transpiler.ts
@@ -1,31 +1,35 @@
 import path from "path";
 import fs from "fs";
+
 const transpiler = new Bun.Transpiler({
     loader: "tsx",
 });
 
-const dir = path.join(import.meta.dir, "..", "assets", "scripts");
-const scripts = fs.readdirSync(dir).filter((file) => file.endsWith(".ts"));
-for (const script of scripts) {
-    const file = fs.readFileSync(path.join(dir, script), "utf-8");
-    const result = transpiler.transformSync(file);
-    if (result) {
-        console.log(`Transpiled ${script} > ${script.replace(".ts", ".js")}`);
-        fs.writeFileSync(path.join(dir, script.replace(".ts", ".js")), result);
-    } else {
-        console.error(`Failed to transpile ${script}`);
+function transpileDirectory(sourceDir: string) {
+    const scripts = fs.readdirSync(sourceDir).filter((file) => file.endsWith(".ts"));
+
+    for (const script of scripts) {
+        const filePath = path.join(sourceDir, script);
+        const fileContent = fs.readFileSync(filePath, "utf-8");
+        const result = transpiler.transformSync(fileContent);
+
+        if (result) {
+            const outputFile = path.join(sourceDir, script.replace(".ts", ".js"));
+            console.log(`Transpiled ${script} > ${path.basename(outputFile)}`);
+            fs.writeFileSync(outputFile, result);
+        } else {
+            console.error(`Failed to transpile ${script}`);
+        }
     }
 }
 
-const dir2 = path.join(import.meta.dir, "..", "webserver", "www", "game", "js");
-const scripts2 = fs.readdirSync(dir2).filter((file) => file.endsWith(".ts"));
-for (const script of scripts2) {
-    const file = fs.readFileSync(path.join(dir2, script), "utf-8");
-    const result = transpiler.transformSync(file);
-    if (result) {
-        console.log(`Transpiled ${script} > ${script.replace(".ts", ".js")}`);
-        fs.writeFileSync(path.join(dir2, script.replace(".ts", ".js")), result);
-    } else {
-        console.error(`Failed to transpile ${script}`);
-    }
+// Define directories to transpile
+const directories = [
+    path.join(import.meta.dir, "..", "assets", "scripts"),
+    path.join(import.meta.dir, "..", "webserver", "www", "game", "js"),
+];
+
+// Transpile each directory
+for (const dir of directories) {
+    transpileDirectory(dir);
 }
